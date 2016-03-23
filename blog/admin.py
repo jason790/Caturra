5f28ad170df90dd6774c75135660ce9bdc563bee
Shavit <stmmsn@gmail.com>
2016-03-23 17:37:31 +0200

    deleting the blog

--- blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# from adminsortable.admin import SortableAdmin, SortableTabularInline
-# from cms.admin.placeholderadmin import PlaceholderAdminMixin
-# from easy_select2 import select2_modelform
-from .models import Post
-
-# Register your models here.
-admin.site.register(Post)
