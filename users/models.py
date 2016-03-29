e79fc8e3a1e684a7857b55dc8083b9a786b8bdac
Shavit <stmmsn@gmail.com>
2016-03-29 18:41:06 +0300

    describe users 🗒

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+
+    picture = models.CharField(max_length=20)
+    description = models.CharField(max_length=20)
+
+    email = models.CharField(max_length=80)
+    password = models.CharField(max_length=140)
+
+    gender = models.CharField(max_length=6)
+    birth = models.DateTimeField('date birth')
+
+    subscription = models.CharField(max_length=20)
+
+    created_at = models.DateTimeField('date created')
+    updated_at = models.DateTimeField('date updated')
