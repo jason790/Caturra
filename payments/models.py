0339e0b2d7926a4ccd9a41d3774e65167d35ada5
Shavit Tzuriel <stmmsn@gmail.com>
2016-03-29 14:17:52 +0300

    starting with the model integration ⚙

--- payments/models.py
+++ payments/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.db.models import Model
+
+# for django cms
+from cms.models import CMSPlugin
+
+# Paypal integration
 import paypalrestsdk
 
 paypalrestsdk.configure({
@@ -6,3 +12,7 @@ paypalrestsdk.configure({
     'client_id': os.environ.get('PAYPAL_CLIENT_ID'),
     'client_id': os.environ.get('PAYPAL_CLIENT_SECRET')
 })
+
+class Payment(models.Model):
+    def __init__(self):
+        pass
