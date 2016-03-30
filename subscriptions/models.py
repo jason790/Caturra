c5adbb81605253eab7699a53e1ba9cd69440abae
Shavit Tzuriel <stmmsn@gmail.com>
2016-03-30 21:17:21 +0300

    migrations 🔌

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Subscription(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+
+    price = models.IntegerField(default=0)
+
+    created_at = models.DateTimeField('date created')
+    updated_at = models.DateTimeField('date updated')
