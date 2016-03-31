910dd7de5f14be7bb1d6c397afa41eb800e41a8b
Shavit Tzuriel <stmmsn@gmail.com>
2016-03-31 19:11:15 +0300

    adding subscriptions to the menu

--- payments/admin.py
+++ payments/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from .models import Payment
+from .models import Subscription
 
 # Register your models here.
 admin.site.register(Payment)
+admin.site.register(Subscription)
